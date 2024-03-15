3e110c8d65c8a9b82b87812e3034177c8b73c1a4
mimimilk0723 <alex11300614@gmail.com>
2024-03-15 11:41:18 +0800

    789

---
+++ week04/week04-5java.cpp
@@ -0,0 +1,17 @@
+///week04-5.cpp CPE-0102A, 要看不同的排版法
+///ALLman(ANSI), Java,K&R(C發明者),Stroustrup(C++發明者)
+#include <stdio.h>
+int main() {
+    int n,a,b;
+    scanf("%d",&n); ///有n筆資料
+    for(int i=0; i<n; i++) { ///迴圈跑幾次
+        scanf("%d",&a); ///數字a: 還有a個數字
+        int ans=0; ///迴圈前面int ans=0
+        for(int j=0; j<a; j++) { ///讀入a個數字
+            scanf("%d",&b);///讀入數字b
+            ans += b;///迴圈裡面 ans+=b
+
+        }
+        printf("%d\n",ans);///迴圈後面,把答案印出來
+    }
+}
