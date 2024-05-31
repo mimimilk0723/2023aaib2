e29d08d75d1d8254785c3998d69ee6bc7d229fc2
mimimilk0723 <145088284+mimimilk0723@users.noreply.github.com>
2024-05-31 11:41:49 +0800

    Delete week15-3.c

--- week15-3.c
+++
@@ -1,9 +0,0 @@
-int equalSubstring(char* s, char* t, int maxCost) {
-    int ans=0;
-    int N=strlen(s); ///先知道字串長度
-    for(int i=0;i<N;i++){  ///字串的迴圈
-    ("%c%c\n",s[i],t[i]); ///字串的陣列
-    }
-    return ans; ///課堂作業1,嫌隨便給個0當答案(答案不對)
-    
-}
